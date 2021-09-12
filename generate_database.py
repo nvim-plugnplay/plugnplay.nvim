c467a5a1ce5ea467ec4c9149b01be5dd5c7c0436
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-09-12 16:53:04 -0400

    fix(generate_database): proper syntax for generated database
      - Update database file

--- generate_database.py
+++ generate_database.py
@@ -10,6 +10,9 @@ client_id = None
 client_secret = None
 
 with open("database.json", "+w") as file:
+    # Save all plugins stuff in a dictionary
+    plugins = {}
+
     print("Reading through line", count)
     for line in r.iter_lines():
         match = re.search(rb"-\s+\[([^/]+/[^]]+)\]\(([^)]+)\)\s+-\s+(.+)", line)
@@ -24,11 +27,11 @@ with open("database.json", "+w") as file:
             req_json = json.loads(req.text)
             # Extract only the data that we need
             plugin_data = {}
+            plugin_name = ""
             wanted_fields = ["full_name", "description", "default_branch", "fork", "archived", "private", "clone_url", "commits_url", "created_at", "updated_at", "stargazers_count", "subscribers_count", "forks_count", "language", "open_issues_count"]
             for field in req_json.keys():
                 if field == "name":
-                    # Do magic that manipulates the JSON object here
-                    pass
+                    plugin_name = req_json[field]
                 if field in wanted_fields:
                     plugin_data[field] = req_json[field]
 
@@ -56,10 +59,11 @@ with open("database.json", "+w") as file:
             # Remove not needed stuff from plugin data and merge plugin and commit data
             del plugin_data["commits_url"]
             plugin_data = {**plugin_data, **commit_data}
-
-            file.write(json.dumps(plugin_data, sort_keys=True, indent=4))
-            count += 1
+            plugins = {**plugins, f"{plugin_name}": {**plugin_data}}
             print("---- DUMPED JSON ----")
+            count += 1
+
+    file.write(json.dumps(plugins, sort_keys=True, indent=4))
     file.close()
 
 print("Success! Dumped", count, "repos!")

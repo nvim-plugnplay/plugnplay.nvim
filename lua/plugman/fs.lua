36fcbe6d7b2ac3e3f80eda038b0c833640200d80
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-09-12 10:19:56 -0400

    refact(fs): move file variable, properly close file if not exists before writing to it

--- lua/plugman/fs.lua
+++ lua/plugman/fs.lua
@@ -10,9 +10,9 @@ function fs.file_exists(location)
 end
 
 function fs.read_or_create(location, default)
-    local file, content
+    local content
     if fs.file_exists(location) then
-        file = vim.loop.fs_open(location, "r", 438)
+        local file = vim.loop.fs_open(location, "r", 438)
         local stat = vim.loop.fs_fstat(file)
         content = vim.loop.fs_read(file, stat.size, 0)
         vim.loop.fs_close(file)
@@ -20,11 +20,12 @@ function fs.read_or_create(location, default)
         content = vim.trim(default)
         -- 644 sets read and write permissions for the owner, and it sets read-only
         -- mode for the group and others
-        file = vim.loop.fs_open(location, "w+", tonumber("644", 8), function(err, fd)
+        vim.loop.fs_open(location, "w+", tonumber("644", 8), function(err, file)
             if not err then
                 local file_pipe = vim.loop.new_pipe(false)
-                vim.loop.pipe_open(file_pipe, fd)
+                vim.loop.pipe_open(file_pipe, file)
                 vim.loop.write(file_pipe, content)
+                vim.loop.fs_close(file)
             end
         end)
     end

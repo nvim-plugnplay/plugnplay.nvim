167c5e453f51be2b6459e535c50fff1227e92bb2
Vhyrro <vhyrro@gmail.com>
2021-09-12 21:42:59 +0200

    feat: generate database

--- generate_database.py
+++ generate_database.py
@@ -4,7 +4,10 @@ r = requests.get("https://raw.githubusercontent.com/rockerBOO/awesome-neovim/mai
 
 print("Pulled awesome-neovim README")
 
-count = 0
+count = 1
+
+client_id = None
+client_secret = None
 
 with open("database.json", "+w") as file:
     print("Reading through line", count)
@@ -12,14 +15,46 @@ with open("database.json", "+w") as file:
         match = re.search(rb"-\s+\[([^/]+/[^]]+)\]\(([^)]+)\)\s+-\s+(.+)", line)
         if match:
             print("Match found for", match.group(1))
-            req = requests.get(b"https://api.github.com/repos/" + match.group(1))
-
+            req = requests.get(b"https://api.github.com/repos/" + match.group(1), auth=(client_id, client_secret))
             if req.text.find("API rate limit exceeded") != -1:
                 print("We are being rate limited!")
                 print(req.text)
                 exit(-1)
 
-            file.write(json.dumps(json.loads(req.text), sort_keys=True, indent=4))
+            req_json = json.loads(req.text)
+            # Extract only the data that we need
+            plugin_data = {}
+            wanted_fields = ["full_name", "name", "description", "default_branch", "fork", "archived", "private", "clone_url", "commits_url", "created_at", "updated_at", "stargazers_count", "subscribers_count", "forks_count", "language", "open_issues_count"]
+            for field in req_json.keys():
+                if field in wanted_fields:
+                    plugin_data[field] = req_json[field]
+
+            # Get the latest commit relevant information
+            if not "commits_url" in plugin_data:
+                print("No commit URL found for", match.group(1), "- skipping...")
+                continue
+
+            req = requests.get(plugin_data["commits_url"], auth=(client_id, client_secret))
+            if req.text.find("API rate limit exceeded") != -1:
+                print("We are being rate limited!")
+                print(req.text)
+                exit(-1)
+
+            req_json = json.loads(req.text)
+            commit_data = {}
+            wanted_fields = ["sha", "commit"]
+            for field in req_json.keys():
+                if field in wanted_fields:
+                    if field == "commit":
+                        commit_data["commit_date"] = req_json["commit"]["author"]["date"]
+                    else:
+                        commit_data[field] = req_json[field]
+
+            # Remove not needed stuff from plugin data and merge plugin and commit data
+            del plugin_data["commits_url"]
+            plugin_data = {**plugin_data, **commit_data}
+
+            file.write(json.dumps(plugin_data, sort_keys=True, indent=4))
             count += 1
             print("---- DUMPED JSON ----")
     file.close()

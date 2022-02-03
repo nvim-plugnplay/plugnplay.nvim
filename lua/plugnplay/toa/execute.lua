fb005e8d24bcd88378fa2a8619eb0d5fd59f793b
max397574 <max.heinzer397@gmx.ch>
2022-02-03 21:45:08 +0100

    feat(toa/execute): get install_action with util function

--- lua/plugnplay/toa/execute.lua
+++ lua/plugnplay/toa/execute.lua
@@ -6,22 +6,33 @@ local toa = {
         name = "startup-nvim",
     },
     {
+        action = "install",
+        name = "better-escape",
+    },
+    {
         action = "remove",
         name = "neorg",
     },
 }
+
+local config = require("plugnplay").config
 
 local function action_remove(action)
     print("removed " .. action.name)
 end
 
 local function action_install(action)
-    print("installed " .. action.name)
+    local install_action
+    if type(config.plugins[action.name]) == "string" then
+        install_action = require("plugnplay.utils").get_install_action(config.plugins[action.name])
+    else
+        install_action = require("plugnplay.utils").get_install_action(config.plugins[action.name].url)
+    end
+    dump(install_action)
 end
 
 function toa_exec.execute(table_of_act)
-    print("table_of_act:")
-    dump(table_of_act)
+    print(vim.inspect(table_of_act))
     for _, action in ipairs(table_of_act) do
         if action.action == "remove" then
             action_remove(action)
@@ -31,6 +42,8 @@ function toa_exec.execute(table_of_act)
     end
 end
 
-toa_exec.execute(toa)
+function toa_exec.test()
+    toa_exec.execute(toa)
+end
 
 return toa_exec

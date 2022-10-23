cf0d8ec4e4c9fa5e490c2f1766fd4f50d613e49f
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-10-23 17:22:39 -0400

    chore: small doc updates

--- lua/plugnplay/helpers.lua
+++ lua/plugnplay/helpers.lua
@@ -2,7 +2,7 @@ local helpers = {}
 
 ---Repeats a value `x` times
 ---@param value string The value to repeat
----@param x  number How many times to repeat the value
+---@param x     number How many times to repeat the value
 function helpers.rep(value, x)
     if x == 1 then
         return value
@@ -13,8 +13,8 @@ end
 
 ---Checks if the neovim version is newer than a specified version
 ---@param major number The minimum major version
----@param minor  number The minimum minor version
----@param patch  number The minimum patch version
+---@param minor number The minimum minor version
+---@param patch number The minimum patch version
 function helpers.is_minimum_version(major, minor, patch)
     local version = vim.version()
 
